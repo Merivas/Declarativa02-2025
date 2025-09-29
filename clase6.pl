0475d996a9c3a53e2905edf3be51b6da4187631d
Merivas <merivas@uca.edu.sv>
2025-09-29 13:13:18 -0600

    update class

--- clase6.pl
+++ clase6.pl
@@ -1,75 +1,56 @@
 % Árbol binario como término:
-%arbol(Valor, Izquierdo, Derecho).
+% arbol(Valor, Izquierdo, Derecho).
 
 % Ejemplo:
 arbol(a,
       arbol(b, arbol(d,nil,nil), arbol(e,nil,nil)),
       arbol(c,nil,nil)).
 
-% verificar si es un arbol 
-
+% Verificar si es un árbol
 es_arbol(nil).
 es_arbol(arbol(_,Izq,Der)) :-
     es_arbol(Izq),
     es_arbol(Der).
 
-      %Recorridos
+% Recorridos
 
-      %Inorden (Izq – Raíz – Der):
-      inorden(nil, []). %Si el árbol está vacío (nil), el recorrido no tiene elementos → lista vacía [].
-      inorden(arbol(X,Izq,Der), L) :-
-      inorden(Izq,L1),
-      inorden(Der,L2),
-      append(L1,[X|L2],L).
+% Inorden (Izq - Raíz - Der):
+inorden(nil, []). % Si el árbol está vacío (nil), el recorrido no tiene elementos -> lista vacía [].
+inorden(arbol(X,Izq,Der), L) :-
+    inorden(Izq,L1),
+    inorden(Der,L2),
+    append(L1,[X|L2],L).
 
-      % Preorden (Raíz – Izq – Der)
-      preorden(nil, []).
-      preorden(arbol(X,Izq,Der), L) :-
-      preorden(Izq, L1),
-      preorden(Der, L2),
-      append([X|L1], L2, L).
+% Preorden (Raíz - Izq - Der)
+preorden(nil, []).
+preorden(arbol(X,Izq,Der), L) :-
+    preorden(Izq, L1),
+    preorden(Der, L2),
+    append([X|L1], L2, L).
 
-      % Postorden (Izq – Der – Raíz)
-      postorden(nil, []). 
-      postorden(arbol(X,Izq,Der), L) :-
-      postorden(Izq, L1),
-      postorden(Der, L2),
-      append(L1, L2, L3), %Concatena listas
-      append(L3, [X], L).
-
-
-%inorden(arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil)),L).
+% Postorden (Izq - Der - Raíz)
+postorden(nil, []).
+postorden(arbol(X,Izq,Der), L) :-
+    postorden(Izq, L1),
+    postorden(Der, L2),
+    append(L1, L2, L3), % Concatena listas
+    append(L3, [X], L).
 
 
-% busqueda de una arbol
+% Buscar un valor dentro de un árbol
 buscar(X, arbol(X,_,_)).
 buscar(X, arbol(_,Izq,_)) :- buscar(X,Izq).
 buscar(X, arbol(_,_,Der)) :- buscar(X,Der).
 
-%buscar(e, arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil))).
-
-%consultas 
-% es_arbol(nil).
-
-% es_arbol(arbol(a,nil,nil)).
-
-% 
-
-
-% es_arbol(arbol(a, b, nil)).
-% porque b no es un arbol
-
+% Consultas de ejemplo
+% inorden(arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil)),L).
 % buscar(e, arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil))).
-
 % buscar(c, arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil))).
-
 % buscar(z, arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil))).
-
 % buscar(X, arbol(a,arbol(b,arbol(d,nil,nil),arbol(e,nil,nil)),arbol(c,nil,nil))).
 
 
-
-            % Definición del árbol (arbol(Raíz, Izq, Der))
+% Definición del árbol (arbol(Raíz, Izq, Der))
 arbol_nordico(
     arbol(odin,
         arbol(frigg,
@@ -88,14 +69,6 @@ arbol_nordico(
         nil
     )
 ).
-
-% Recorrido en postorden (Izq – Der – Raíz)
-postorden(nil, []).
-postorden(arbol(X, Izq, Der), L) :-
-    postorden(Izq, L1),
-    postorden(Der, L2),
-    append(L1, L2, L3),
-    append(L3, [X], L).
 
 % arbol_nordico(A), postorden(A, L).
 
@@ -119,13 +92,6 @@ arbol_mitologia(
         nil
     )
 ).
-% Recorrido en preorden (Raíz – Izq – Der)
-preorden(nil, []).
-preorden(arbol(X, Izq, Der), L) :-
-    preorden(Izq, L1),
-    preorden(Der, L2),
-    append([X|L1], L2, L).
-
 % arbol_mitologia(A), preorden(A, L).
 
 :- preorden(
@@ -165,4 +131,3 @@ preorden(arbol(X, Izq, Der), L) :-
            nil
        ),
        L).
-
